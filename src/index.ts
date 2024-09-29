0f45ab68569f1914a6f3b1347074bb17ff5ec90f
Gregg Schofield <schofieldgregg@gmail.com>
2024-09-29 21:10:36 +0100

    feat: add PlatformQueue construct

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import {Construct} from "constructs";
+import {aws_sqs} from "aws-cdk-lib";
+
+export class PlatformQueue extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new aws_sqs.Queue(this, 'PlatformQueue', {
+        fifo: true,
+    });
   }
 }
