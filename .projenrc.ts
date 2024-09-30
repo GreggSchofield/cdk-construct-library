61100b36c5dff86942e785738ee4e561e4e8a9eb
Gregg Schofield <schofieldgregg@gmail.com>
2024-09-30 08:08:31 +0100

    fix: stop publishing go bindings

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,10 +9,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
   projenrcTs: true,
   repositoryUrl: 'git@github.com:GreggSchofield/cdk-construct-library.git',
 
-  publishToGo: {
-    moduleName: 'github.com/GreggSchofield/cdk-construct-library-go',
-  }
-
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
