38d2bf23ae9b5ce60f6298c52a43fe90215fd16d
Gregg Schofield <schofieldgregg@gmail.com>
2024-10-07 11:51:53 +0100

    reorder imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { aws_sqs } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
 import { DeadLetterQueue } from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
 
 export interface PlatformQueueProps {
   readonly fifo?: boolean;
