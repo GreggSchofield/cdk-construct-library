62f683ccccd72ff7301366e5bcff0fceb39a9dbf
Gregg Schofield <schofieldgregg@gmail.com>
2024-10-07 11:40:22 +0100

    remove tsdoc

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,8 @@ import { aws_sqs } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {DeadLetterQueue} from "aws-cdk-lib/aws-sqs";
 
-/**
- * Properties for the  PlatformQueue Construct.
- */
 export interface PlatformQueueProps {
-
-  /**
-   * Create a first-in first-out (fifo) queue.
-   */
   readonly fifo?: boolean,
-
-  /**
-   * Create a dead letter queue for failed messages.
-   */
   readonly dlq?: boolean,
 }
 
@@ -27,9 +16,6 @@ function createDeadLetterQueue(ctx: PlatformQueue): DeadLetterQueue {
   return { maxReceiveCount: 1, queue: dlq };
 }
 
-/**
- * Construct for an opinionated AWS SQS Queue.
- */
 export class PlatformQueue extends Construct {
   constructor(scope: Construct, id: string, props: PlatformQueueProps) {
     super(scope, id);
