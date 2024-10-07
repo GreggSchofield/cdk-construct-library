02be92ebc99858ff7b08530664cb6c95bfb4c240
Gregg Schofield <schofieldgregg@gmail.com>
2024-10-07 09:44:50 +0100

    add tsdoc for the PlatformQueue construct

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { aws_sqs } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+/**
+ * An AWS SQS Queue that is secure by default.
+ */
 export class PlatformQueue extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
