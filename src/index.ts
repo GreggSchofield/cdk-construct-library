4ed944fcbeec8350149fcd544bbcd0fbda0c83ba
Gregg Schofield <schofieldgregg@gmail.com>
2024-10-07 11:26:35 +0100

    add fifo queue feature

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,42 @@
 import { aws_sqs } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {DeadLetterQueue} from "aws-cdk-lib/aws-sqs";
 
+/**
+ * Properties for the  PlatformQueue Construct.
+ */
+export interface PlatformQueueProps {
+
+  /**
+   * Create a first-in first-out (fifo) queue.
+   */
+  fifo?: boolean,
+
+  /**
+   * Create a dead letter queue for failed messages.
+   */
+  dlq?: boolean,
+}
+
+/**
+ * @jsii ignore
+ */
+function createDeadLetterQueue(ctx: PlatformQueue): DeadLetterQueue {
+  const dlq = new aws_sqs.Queue(ctx, 'DeadLetterQueue',);
+
+  return { maxReceiveCount: 1, queue: dlq };
+}
+
+/**
+ * Construct for an opinionated AWS SQS Queue.
+ */
 export class PlatformQueue extends Construct {
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string, props: PlatformQueueProps) {
     super(scope, id);
 
     new aws_sqs.Queue(this, 'PlatformQueue', {
-      fifo: true,
+      fifo: props.fifo,
+      deadLetterQueue: props.dlq ? createDeadLetterQueue(this) : undefined,
     });
   }
 }
--- test/hello.test.ts
+++ test/hello.test.ts
@@ -3,15 +3,23 @@ import { Template } from 'aws-cdk-lib/assertions';
 import { PlatformQueue } from '../src';
 
 test('An single AWS SQS Queue is created', () => {
-
   const app = new cdk.App();
-
   const stack = new cdk.Stack(app, 'TestStack');
-
-  new PlatformQueue(stack, 'TestPlatformQueue');
+  new PlatformQueue(stack, 'TestPlatformQueue', {});
 
   const template = Template.fromStack(stack);
 
   template.resourceCountIs('AWS::SQS::Queue', 1);
+});
 
+test('An single AWS SQS Queue is created with DLQ', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, 'TestStack');
+  new PlatformQueue(stack, 'TestPlatformQueue', {
+    dlq: true,
+  });
+
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::SQS::Queue', 2);
 });
